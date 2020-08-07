9837768cfb3f497baafac044279921053625c70f
Matthew <mattwarner@rogers.com>
2020-08-06 21:18:05 -0400

    Partially fixes the model rotation. Its still not fully working as expected.

--- lib/GS/GS_TransformComponent.cpp
+++ lib/GS/GS_TransformComponent.cpp
@@ -20,10 +20,35 @@ void
 TransformComponent::onGUI()
 {
     glm::vec3 euler_rotation = glm::eulerAngles(m_rotation);
+    euler_rotation = glm::degrees(euler_rotation);
 
     ImGui::DragFloat3("Position", glm::value_ptr(m_position));
-    if (ImGui::DragFloat3("Rotation", glm::value_ptr(euler_rotation)))
-        m_rotation = glm::toQuat(glm::orientate3(euler_rotation));
+    if (ImGui::DragFloat3("Rotation", glm::value_ptr(euler_rotation), 1.0f))
+    {
+        if (euler_rotation.y >= 89.0f)
+            euler_rotation.y -= 89.0f * 2.0f;
+        else if (euler_rotation.y <= -89.0f)
+            euler_rotation.y += 89.0f * 2.0f;
+
+        if (euler_rotation.x >= 180.0f)
+            euler_rotation.x -= 360.0f;
+        else if (euler_rotation.x <= 180.0f)
+            euler_rotation.x += 360.0f;
+
+        if (euler_rotation.z >= 180.0f)
+            euler_rotation.z -= 360.0f;
+        else if (euler_rotation.z <= 180.0f)
+            euler_rotation.z += 360.0f;
+
+        glm::quat q_x_axis = glm::angleAxis(
+                glm::radians(euler_rotation.x), glm::vec3{1.0f, 0.0f, 0.0f});
+        glm::quat q_y_axis = glm::angleAxis(
+                glm::radians(euler_rotation.y), glm::vec3{0.0, 1.0f, 0.0f});
+        glm::quat q_z_axis = glm::angleAxis(
+                glm::radians(euler_rotation.z), glm::vec3{0.0f, 0.0f, 1.0f});
+
+        m_rotation = q_z_axis * q_y_axis * q_x_axis;
+    }
     ImGui::DragFloat3("Scale", glm::value_ptr(m_scale));
 }
 glm::mat4
